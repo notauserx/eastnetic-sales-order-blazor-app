98440c91437731cd039fe63dca462a422ce9f421
notauserx <82922717+notauserx@users.noreply.github.com>
2023-05-04 11:19:32 +0600

    add validation

--- src/SalesOrders.Contracts/Response/Order.cs
+++ src/SalesOrders.Contracts/Response/Order.cs
@@ -1,11 +1,16 @@
-﻿using SalesOrders.Core.Enums;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace SalesOrders.Contracts.Response;
 
 public class OrderItem
 {
     public Guid Id { get; set; }
+    
+    [Required]
+    [StringLength(20, ErrorMessage = "Order name cannot be more than 20 characters")]
     public string? Name { get; set; }
+    
+    [Required]
     public string State { get; set; }
     public List<WindowItem>? Windows { get; set; }
 }
